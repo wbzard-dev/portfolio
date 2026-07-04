1b73d5c542f643f39cb7dda54992c089b8049ba3
vivekweberon <vivek@weberon.net>
2026-07-04 23:26:00 +0530

    revamp

--- src/App.jsx
+++ src/App.jsx
@@ -35,11 +35,11 @@ function App() {
 
     useEffect(() => {
         const lenis = new Lenis({
-            duration:        1.2,
+            duration:        1.8,
             easing:          (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             smooth:          true,
             syncTouch:       true,
-            touchMultiplier: 2,
+            touchMultiplier: 1.4,
         });
 
         const raf = (time) => { lenis.raf(time); requestAnimationFrame(raf); };
